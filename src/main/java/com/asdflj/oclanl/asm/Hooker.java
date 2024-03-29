378917279f41ae552af48c67cff6dff67cffb222
zy <854865755@163.com>
2024-03-29 15:30:37 +0800

    fix bug

--- src/main/java/com/asdflj/oclanl/asm/Hooker.java
+++ src/main/java/com/asdflj/oclanl/asm/Hooker.java
@@ -3,12 +3,14 @@ package com.asdflj.oclanl.asm;
 import java.net.InetAddress;
 
 import li.cil.oc.Settings;
+import li.cil.oc.server.component.InternetCard$;
 
-public final class Hooker {
+public class Hooker {
 
     public static boolean isRequestAllowed(Settings settings, InetAddress inetAddress, String host) {
         return inetAddress.isAnyLocalAddress() || inetAddress.isLoopbackAddress()
             || inetAddress.isLinkLocalAddress()
-            || inetAddress.isSiteLocalAddress();
+            || inetAddress.isSiteLocalAddress()
+            || InternetCard$.MODULE$.isRequestAllowed(settings, inetAddress, host);
     }
 }
--- src/main/java/com/asdflj/oclanl/asm/InternetCardTransformer.java
+++ src/main/java/com/asdflj/oclanl/asm/InternetCardTransformer.java
@@ -1,6 +1,7 @@
 package com.asdflj.oclanl.asm;
 
 import org.objectweb.asm.ClassVisitor;
+import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
@@ -13,6 +14,11 @@ public class InternetCardTransformer extends OCClassTransformer.ClassMapper {
         return new TransformInternetCard(Opcodes.ASM5, downstream);
     }
 
+    @Override
+    protected int getWriteFlags() {
+        return ClassWriter.COMPUTE_FRAMES;
+    }
+
     private static class TransformInternetCard extends ClassVisitor {
 
         TransformInternetCard(int api, ClassVisitor cv) {
@@ -21,7 +27,7 @@ public class InternetCardTransformer extends OCClassTransformer.ClassMapper {
 
         @Override
         public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-            if ("isRequestAllowed".equals(name)) {
+            if ("checkLists".equals(name)) {
                 return new TransformIsRequestAllowed(api, super.visitMethod(access, name, desc, signature, exceptions));
             }
             return super.visitMethod(access, name, desc, signature, exceptions);
@@ -34,30 +40,18 @@ public class InternetCardTransformer extends OCClassTransformer.ClassMapper {
             super(api, mv);
         }
 
-        private boolean allow = true;
-
         @Override
-        public void visitInsn(int opcode) {
-            if (opcode == Opcodes.ICONST_0) {
-                allow = false;
-            } else if (opcode == Opcodes.IRETURN && !allow) {
-                super.visitVarInsn(Opcodes.ALOAD, 1);
-                super.visitVarInsn(Opcodes.ALOAD, 2);
-                super.visitVarInsn(Opcodes.ALOAD, 3);
+        public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
+            if (name.equals("isRequestAllowed")) {
                 super.visitMethodInsn(
                     Opcodes.INVOKESTATIC,
                     "com/asdflj/oclanl/asm/Hooker",
                     "isRequestAllowed",
                     "(Lli/cil/oc/Settings;Ljava/net/InetAddress;Ljava/lang/String;)Z",
                     false);
-                allow = true;
+            } else {
+                super.visitMethodInsn(opcode, owner, name, desc, itf);
             }
-            super.visitInsn(opcode);
-        }
-
-        @Override
-        public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
-            super.visitMethodInsn(opcode, owner, name, desc, itf);
         }
     }
 
